b54e73b8c11fd61da4ea26230a03e667e1d17fa6
Jonathan Tutterow <jtutterow@outlook.com>
2021-12-08 17:18:27 -0600

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+    let repeatedWord = "";
 
+    if(count < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < count; i++){
+        repeatedWord = repeatedWord + word;
+    }
+    return repeatedWord;
 };
 
 // Do not edit below this line

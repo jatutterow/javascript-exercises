2f518a5ffc59ed4928f61c8c7164e51e9c8419f8
Jonathan Tutterow <jtutterow@outlook.com>
2021-12-14 23:38:29 -0600

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(source, ...remove) {
+    for(let i = 0; i < remove.length; i++){
+        for(let j = 0; j < source.length; j++){
+            if(remove[i] === source[j]){
+                source.splice(j, 1)
+            }
+        }
+    }
+    return source;
 };
 
 // Do not edit below this line

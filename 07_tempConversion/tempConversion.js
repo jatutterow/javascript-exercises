6e323939312a9cff40960ebfacab776aa863a224
Jonathan Tutterow <jtutterow@outlook.com>
2021-12-15 00:02:11 -0600

    Completed exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fTemp) {
+  let cTemp = (fTemp - 32) * 5 / 9;
+  return cTemp.toPrecision(1); 
 };
 
-const ctof = function() {
-
+const ctof = function(cTemp) {
+  let fTemp = (cTemp + 32) * 9 / 5;
+  return fTemp.toPrecision(1);
 };
 
 // Do not edit below this line

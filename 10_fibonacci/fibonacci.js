9a6234abd7dff6d3f45c9c4d09d00b291f6bb282
Jonathan Tutterow <jtutterow@outlook.com>
2022-01-24 10:17:51 -0600

    Completed Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if(n <= 0){
+        return "OOPS";
+    }
+    n = Number(n)
+    let n0 = 1;
+    let n1 = 1;
+    for(i = 3; i <= n; i++){
+        let temp = n0 + n1;
+        n0 = n1;
+        n1 = temp;
+    }
+    return n1;
 };
 
 // Do not edit below this line

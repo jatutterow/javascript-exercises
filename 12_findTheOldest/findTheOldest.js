7d5d53e420e1859bfb951dbd2070e0599d27a538
Jonathan Tutterow <jtutterow@outlook.com>
2022-01-24 11:37:54 -0600

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((currOldest, person) => {
+        if(age(person) > age(currOldest)){
+            return person;
+        }
+        return currOldest;
+    });
 };
+
+const age = function(person){
+    date = new Date();
+    let birth = person.yearOfBirth;
+    let death = person.yearOfDeath
+    if(death == undefined){
+        death = date.getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
